7d6dab482727bc8f201cb7c5c669ea6c6426da44
swatimishra62 <swati.mishra@itsoftlab.com>
2025-07-15 16:39:13 +0530

    work on contact page

--- src/app/admin/contacts/page.tsx
+++ src/app/admin/contacts/page.tsx
@@ -23,7 +23,7 @@ export default function AdminContacts() {
 
   return (
     <div className="max-w-4xl mx-auto p-4 sm:p-8">
-      <h2 className="text-2xl font-bold mb-6">Contact Form Details</h2>
+      <h2 className="text-2xl font-bold mb-6">Contact Form </h2>
       {loading ? (
         <div>Loading...</div>
       ) : (
